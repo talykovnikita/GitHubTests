9d9f6950fcd11e76733d6cf8477f2cc195dab2fc
Talykov Nikita <talykovnikita@gmail.com>
2021-05-28 00:36:11 +0300

    Implement 2 tests for SoftAssertions page.

---
+++ src/test/java/githubTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class githubTests {
+    @Test
+    public void checkSoftAssertionsIsPresentInPagesViaSearch(){
+        open("https://github.com/selenide/selenide");
+        $(withText("Wiki")).click();
+        $("#wiki-pages-filter").setValue("Soft");
+        $(withText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+
+    @Test
+    public void checkSoftAssertionsIsPresentInPagesViaExpandList(){
+        open("https://github.com/selenide/selenide");
+        $(withText("Wiki")).click();
+        $(withText("more pages")).click();
+        $(withText("SoftAssertions")).shouldBe(visible);
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
