3a6ba5a4cc295d71d1b7800f0780338ccfb4679f
Roman-1990 <84563341+Roman-1990@users.noreply.github.com>
2021-06-11 11:51:39 +0300

    Add gitBoss

--- src/test/java/testDifferenceBetweenSelectors/testSelectors.java
+++ src/test/java/testDifferenceBetweenSelectors/testSelectors.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 
 public class testSelectors {
-
+    
+public static void gitBoss(String[] args) {
+    System.out.println("Это вторжение:)"));
+    }
+    
     @Test
     public void test1(){
         open("http://localhost:63342/GitHubTests/GitHubTests.test/html/index.html?_ijt=hinun3gbjnkvf0tbt9ipeqo65q");
